b6e76b94fffd05d3f248903957d747d281ad6cff
Till Reiter <till.reiter@gmail.com>
2014-05-12 09:59:43 +0100

    update pictures and content

--- application.js
+++ application.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-  // $("#portfolio").find("#health");
-  $('.portfolio-item').find('#health').on('mouseenter', function() {
-    // var price = $('<p>From $399.99</p>');
-    // $('#health').before(price);
-    $('#health').remove();
-  });
-});
+// $(document).ready(function() {
+//   // $("#portfolio").find("#health");
+//   $('.portfolio-item').find('#health').on('mouseenter', function() {
+//     // var price = $('<p>From $399.99</p>');
+//     // $('#health').before(price);
+//     $('#health').remove();
+//   });
+// });
 
 
-var links = {
-  counter: 0,
-};
-var dict = [0,1,2,3,4,5,6,7,8,9,"a","b","c"];
+// var links = {
+//   counter: 0,
+// };
+// var dict = [0,1,2,3,4,5,6,7,8,9,"a","b","c"];
 
-function genLink (url) {
+// function genLink (url) {
 
-  links[key] = url;
-  links.counter++
-}
+//   links[key] = url;
+//   links.counter++
+// }
